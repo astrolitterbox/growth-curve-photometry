e131fc5d8c2a545066e2fdcccc525bee98cf761e
Astrolitterbox <opit@hardcore.lt>
2012-04-17 17:51:29 +0200

    some code from sdsspy to convert nmaggies to mags

--- utils.py
+++ utils.py
@@ -82,5 +82,61 @@ def gauss_kern(size, sizey=None):
     g = scipy.exp(-(x**2/float(size)+y**2/float(sizey)))
     return g / g.max()
   
-  
+def nmgy2mag(nmgy, ivar=None):
+    """
+    Taken from http://sdsspy.googlecode.com/hg/sdsspy/util.py
+    Name:
+        nmgy2mag
+    Purpose:
+        Convert SDSS nanomaggies to a log10 magnitude.  Also convert
+        the inverse variance to mag err if sent.  The basic formulat
+        is 
+            mag = 22.5-2.5*log_{10}(nanomaggies)
+    Calling Sequence:
+        mag = nmgy2mag(nmgy)
+        mag,err = nmgy2mag(nmgy, ivar=ivar)
+    Inputs:
+        nmgy: SDSS nanomaggies.  The return value will have the same
+            shape as this array.
+    Keywords:
+        ivar: The inverse variance.  Must have the same shape as nmgy.
+            If ivar is sent, then a tuple (mag,err) is returned.
+
+    Outputs:
+        The magnitudes.  If ivar= is sent, then a tuple (mag,err)
+        is returned.
+
+    Notes:
+        The nano-maggie values are clipped to be between 
+            [0.001,1.e11]
+        which corresponds to a mag range of 30 to -5
+    """
+    nmgy = numpy.array(nmgy, ndmin=1, copy=False)
+
+    nmgy_clip = numpy.clip(nmgy,0.001,1.e11)
+
+    mag = nmgy_clip.copy()
+    mag[:] = 22.5-2.5*log10(nmgy_clip)
+
+    if ivar is not None:
+
+        ivar = numpy.array(ivar, ndmin=1, copy=False)
+        if ivar.shape != nmgy.shape:
+            raise ValueError("ivar must be same shape as input nmgy array")
+
+        err = mag.copy()
+        err[:] = 9999.0
+
+        w=where( ivar > 0 )
+
+        if w[0].size > 0:
+            err[w] = sqrt(1.0/ivar[w])
+
+            a = 2.5/log(10)
+            err[w] *= a/nmgy_clip[w]
+
+        return mag, err
+    else:
+        return mag
+
   

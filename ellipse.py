b5572daa8ea94cf5b21713e7b9150dc3ed34455c
Astrolitterbox <opit@hardcore.lt>
2012-08-10 15:46:59 +0200

    before I screw everything up: working on index trimming, will change the return data structures of ellipse()

--- ellipse.py
+++ ellipse.py
@@ -27,9 +27,10 @@ def ellipse(ra,rb,ang,x0,y0,nPoints=50):
 	co,si=cos(an),sin(an)
 
 	the=np.linspace(0,2*pi,nPoints)
-	X=radm*cos(the)*co-si*radn*sin(the)+xpos
-	
+	X=radm*cos(the)*co-si*radn*sin(the)+xpos	
 	Y=radm*cos(the)*si+co*radn*sin(the)+ypos
+	print X.shape, X.dtype
+	
 	return [np.round(Y,0).astype('int16'), np.round(X, 0).astype('int16')]
 
 
@@ -42,42 +43,39 @@ def get_ellipse_circumference(isoA, axisRatio):
   #print circumference 
   return np.round(circumference, 0)
 
-def draw_ellipse(y0, x0, pa, isoA, axisRatio):
+def draw_ellipse(inputShape, y0, x0, pa, isoA, axisRatio):
 	#print isoA, axisRatio, 'a, axisRatio'
 	nPoints = get_ellipse_circumference(isoA, axisRatio)
 	#return ellipse(10,5,7510,10,300)
 	
-	return ellipse(isoA,isoA*axisRatio,pa,x0,y0,nPoints)
+	return cropCoords(inputShape, ellipse(isoA,isoA*axisRatio,pa,x0,y0,nPoints))
 
 
-#border case, solve later if necessary
 
-def getMaxIndices(inputImage):
-    maxY = inputImage.shape[0]
-    print len(inputImage[0,:]), len(inputImage[:, 0])
-    maxX = inputImage.shape[1]
-    return maxY-1, maxX-1 #indexing starts from 0, so the array indices go to max-1
-
-def getGoodCoords(inputImage, ellipseCoords):
+def cropCoords(inputShape, ellipseCoords):
   #taking care of the boundaries: 
-  #clipping all points on the ellipse whose coordinates > the indices of the input image.
+   #for 2D arrays: shape of the array, list of indices
+  #checks if the indices are between 0 and ylim and xlim, rejects the bad coordinates
+  #returns good indices 
   
-  #goodCoords = np.where((ellipseCoords[0] in np.arange(0, getMaxIndices(inputImage)[0])))
-  maxY = getMaxIndices(inputImage)[0]
-  maxX = getMaxIndices(inputImage)[1]
+  maxY = inputShape[0]
+  maxX = inputShape[1]
   print maxY, maxX, 'maxes' 
-  length = ellipseCoords[0]
-  print 'len', length
-  Ycond = [((0 <= ellipseCoords[0]) & (ellipseCoords[0] <= maxY)) & ((0 <= ellipseCoords[1]) & (ellipseCoords[1]<= maxX))]
-  print ellipseCoords
-  print np.where(cond == True), 'where'
-  goodCoords = ellipseCoords[np.where(cond == True)]
-  print goodCoords
+  length = len(ellipseCoords[0])
+  print 'len', length 
+  coords = zip(ellipseCoords[0], ellipseCoords[1])
+  print coords
+  print '____________'
   
+  
+  exit()
+  
+  cond = np.where([((0 <= ellipseCoords[0]) & (ellipseCoords[0] <= maxY)) & ((0 <= ellipseCoords[1]) & (ellipseCoords[1]<= maxX))])
+  
+  goodCoords = ellipseCoords[cond]
+  
+  return goodCoords
   #goodCoords[0] = ellipseCoords[np.where(where)][0]
-  
- 
-  
   
   
        
@@ -93,14 +91,9 @@ def main():
   inputImage = np.zeros((51, 51))
   
     
-  ellipseCoords = draw_ellipse(25, 25, 0, 26, 1)
+  ellipseCoords = draw_ellipse(inputImage.shape, 26, 25, 0, 26, 1)
   
   print len(ellipseCoords), ellipseCoords[0].shape[0]
-  
-  
-  #goodCoords = np.where((ellipseCoords < 
-  goodCoords = getGoodCoords(inputImage, ellipseCoords)
-  
   inputImage[goodCoords[0], goodCoords[1]] = 1000
   hdu = pyfits.PrimaryHDU(inputImage)
   hdu.writeto('ellipse.fits')

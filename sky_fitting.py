ac79507270f5fb398f5a12510e34b6233cbba1c9
astrolitterbox <sbekeraite@aip.de>
2013-06-06 12:36:38 +0200

    masked, testing

--- sky_fitting.py
+++ sky_fitting.py
@@ -167,6 +167,7 @@ class Photometry():
 	      currentFlux = np.sum(inputImage[currentPixels])
 	      #draw ellipse with masks:
 	      inputImageM = np.ma.masked_array(inputImage, mask=mask)
+	      inputImageM = np.ma.fix_invalid(inputImage, fill_value=0)
 	      currentPixelsM = ellipse.draw_ellipse(inputImage.shape, center[0], center[1], pa, isoA, ba)
 	      NpixM = inputImageM[currentPixelsM].compressed().shape[0]
 	      currentFluxM = np.sum(inputImage[currentPixelsM])

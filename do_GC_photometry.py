47bfe2be47206406d3bd15664292838869278810
Astrolitterbox <opit@hardcore.lt>
2013-06-11 12:59:10 +0200

    new flux

--- do_GC_photometry.py
+++ do_GC_photometry.py
@@ -42,40 +42,44 @@ def calculateFlux(flux, i):
     	print 'full magnitude', mag
 	return mag
 
-for i in range(1, 940):
+for i in range(2, 3):
 	#z = db.dbUtils.getFromDB('z', dbDir+'CALIFA.sqlite', 'mothersample', ' where califa_id = '+str(i))
 	#outerRadius_curr = db.dbUtils.getFromDB('isoA', dbDir+'CALIFA.sqlite', 'sky_fits_'+band, ' where califa_id = '+str(i))[0]
 	#outerRadius_r = db.dbUtils.getFromDB('isoA', dbDir+'CALIFA.sqlite', 'sky_fits_r', ' where califa_id = '+str(i))[0]
 	sky = db.dbUtils.getFromDB('sky', dbDir+'CALIFA.sqlite', 'sky_new', ' where califa_id = '+str(i))[0]
-	skyM = db.dbUtils.getFromDB('sky', dbDir+'CALIFA.sqlite', 'sky_new', ' where califa_id = '+str(i))[0]	
+	skyM = db.dbUtils.getFromDB('skyMasked', dbDir+'CALIFA.sqlite', 'sky_new', ' where califa_id = '+str(i))[0]	
 	print sky, skyM
-	sky = 108.6
+	##sky = 108
 	dataFile = dataDir+"/gc_profile"+str(i)+".csv"
 	data = np.genfromtxt(dataFile)
 	isoA = data[:, 0]
 	currentFlux = data[:, 1] #current flux
 	Npix = data[:, 2] #current flux
 	currentFluxM = data[:, 3] #current flux
-	NpixM = data[:, 4] #current flux
-	print currentFlux[10], Npix[10]
+	NpixM = data[:, 4] 
+	#print np.sum(currentFlux), 'sum curr'
+	#sky+= 2
 	currentFlux = np.subtract(currentFlux, Npix*sky)
-	#currentFlux = sky
-	print currentFlux[10]	
+	
+	#print np.sum(currentFlux), 'sum curr'
+
+	#print currentFlux[1]	
 	cumFlux = np.cumsum(currentFlux) 
 	cumFluxM = np.cumsum(currentFlux)
 	
-	totalFlux = np.sum(currentFlux) #- sky*np.sum(Npix)
-	totalFluxM = np.sum(currentFlux)# - skyM*np.sum(Npix)
-	print totalFlux
+	#totalFlux = np.sum(currentFlux) #- sky*np.sum(Npix)
+	#totalFluxM = np.sum(currentFlux)# - skyM*np.sum(Npix)
+	#print totalFlux, 'total'
+	print cumFlux[-1], 'cum'
 	
-	elMag = calculateFlux(totalFlux, i)
-	elMagM = calculateFlux(totalFluxM, i)
+	elMag = calculateFlux(cumFlux[-1], i)
+	#elMagM = calculateFlux(totalFluxM, i)
 	print elMag, elMagM
-	
+	#exit()	
 	fig = plt.figure()
-	plt.scatter(isoA, currentFlux)
+	plt.scatter(isoA, cumFlux)
 	plt.savefig('img/curves/'+band+"/"+str(i))
-	exit()
+	#exit()
 	
 	#sc_index = np.where(np.round(flux, 0) == round(flux[0]/math.e, 0))
         #sc_index = np.where([(np.divide(centralFlux, flux) > math.e) & (isoA > maxFluxIsoA)])[1][0] #the first index after the maximum	   

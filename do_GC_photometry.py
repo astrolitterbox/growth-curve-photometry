fixed flux estimate, masked points calculation

do_GC_photometry.py
@@ -6,6 +6,8 @@ import math
 import db
 import getTSFieldParameters
 import sys 
+import pyfits
+
 
 #import cosm
 dbDir = '../db/'
@@ -52,87 +54,74 @@ for i in range(lim_lo, lim_hi):
 	#z = db.dbUtils.getFromDB('z', dbDir+'CALIFA.sqlite', 'mothersample', ' where califa_id = '+str(i))
 	#outerRadius_curr = db.dbUtils.getFromDB('isoA', dbDir+'CALIFA.sqlite', 'sky_fits_'+band, ' where califa_id = '+str(i))[0]
 	#outerRadius_r = db.dbUtils.getFromDB('isoA', dbDir+'CALIFA.sqlite', 'sky_fits_r', ' where califa_id = '+str(i))[0]
-	sky = 110#db.dbUtils.getFromDB('sky', dbDir+'CALIFA.sqlite', 'sky_new', ' where califa_id = '+str(i))[0]
+	sky = db.dbUtils.getFromDB('sky', dbDir+'CALIFA.sqlite', 'sky_new', ' where califa_id = '+str(i))[0]
 	skyM = db.dbUtils.getFromDB('skyMasked', dbDir+'CALIFA.sqlite', 'sky_new', ' where califa_id = '+str(i))[0]	
 	print sky, skyM
 	##sky = 108
 	dataFile = dataDir+"/gc_profile"+str(i)+".csv"
-	dataFileOld = dataDirOld+"/gc_profile"+str(i)+".csv"
+	#dataFileOld = dataDirOld+"/test/gc_profile"+str(i)+".csv"
 	data = np.genfromtxt(dataFile)
-	oldData = np.genfromtxt(dataFileOld)
-	newSize = min(data.shape[0], oldData.shape[0])
-	data = data[:newSize,:]
-	oldData = oldData[:newSize,:]
-	isoA = oldData[:, 0]
-	
-	NpixOld = oldData[:, 4]		
-	isoANew = data[:, 0]	
-	currentFlux = data[:, 1] #current flux
-	Npix = data[:, 2] #npix
-	#currentFluxM = data[:, 3] #current flux
-	#NpixM = data[:, 4]
-	#print currentFlux
-	currentFlux = np.subtract(currentFlux, Npix*sky)
-	cumFlux = np.cumsum(currentFlux) 
-	
+	#oldData = np.genfromtxt(dataFileOld)
+	#newSize = min(data.shape[0], oldData.shape[0])
+	#data = data[:newSize,:]
+	#oldData = oldData[:newSize,:]
+	#isoA = oldData[:, 0]
 	#cumFluxOld = oldData[:, 1]
-	print Npix.shape, 'npx'
-	currFluxOld = (oldData[:, 3])
-	
-	#currFluxOld = np.subtract(currFluxOld, NpixOld*sky)
-	cumFluxOld = np.cumsum(currFluxOld)
-	
-	
-	#ell = data[:, 3]
-	print cumFluxOld[-1], 'cfo'
-	#sky+= 2
-	#currentFlux = np.subtract(currentFlux, Npix*sky)
-	
-	#print np.sum(currentFlux), 'sum curr'
+	#NpixOld = oldData[:, 4]		
+	#NpixOldCum = oldData[:, 5]	
+	isoA = data[:, 0]	
+	cumFlux = data[:, 1] #current flux
+	Npix = data[:, 2] #npix
+	totalNpix = Npix[-1]
+	totalFlux = cumFlux[-1]
+	print totalFlux, 'total Flux', totalNpix, 'totalNpix'
+	#maskFile = pyfits.open("ellipseMask6.fits")
+	#mask = maskFile[0].data
 
-	#print currentFlux[1]	
-	#cumFlux = np.cumsum(currentFlux) 
-	#cumFluxM = np.cumsum(currentFlux)
-	#print currFluxOld
+
+	#totalOldNpix = np.sum(mask[np.where(mask == 1)])
 	
-	totalFlux = np.sum(currentFlux)
-	#totalFluxM = np.sum(currentFlux)# - skyM*np.sum(Npix)
-	#print totalFlux, 'total'
-	#print cumFlux[-1], 'cum'
-	elMagOld = calculateFlux(cumFluxOld[-1], i)
-	elMag = calculateFlux(totalFlux, i)
-	print elMagOld, 'old', elMag, 'now'
-	#exit()	
+	totalSky = totalNpix*sky
+	#print totalSky, 'sky sum', totalNpix, 'Npix'
+	skySubFlux = totalFlux - totalSky
+	print skySubFlux, 'sky subtracted flux'
+	elMag = calculateFlux(skySubFlux, i)  
+	print elMag
+	exit()
+	
+	mags.append((i, elMag))
+
+	
 	fig = plt.figure()
 	ax = fig.add_subplot(221)
-	#ax.plot(isoA, cumFluxOld, c="b")
-	ax.plot(isoANew, cumFlux, c="r")
+	ax.plot(isoA, np.cumsum(currentFlux)  - sky*np.cumsum(Npix), c="b")
+	
 
 	ax = fig.add_subplot(222)
-	ax.plot(isoA, cumFluxOld, c="b")	
-	#ax.plot(isoANew, currentFlux, c="r")
+	#ax.plot(isoA, cumFluxOld, c="b")	
+	ax.plot(isoA, currentFlux, c="r")
 	
-	ax = fig.add_subplot(223)
-	ax.plot(isoANew, Npix, c="r")
-	#ax.plot(isoANew, ell, c="b")
+	#ax = fig.add_subplot(223)
+	#ax.plot(isoA, skySubFlux, c="r")
+	#ax.plot(isoANew, cumFluxOld, c="b")
 	
 
-	#mags.append((i, elMag))
+
 	
 	ax = fig.add_subplot(224)
-	ax.plot(isoA, NpixOld, c="b")
+	ax.plot(isoA, Npix, c="r")
 
 	plt.savefig('img/curves/'+band+"/"+str(i))
 	
-	fig = plt.figure()
-	old = db.dbUtils.getFromDB("r_mag", dbDir+'CALIFA.sqlite', 'gc', ' where califa_id < 100')
-	new = db.dbUtils.getFromDB("mag", dbDir+'CALIFA.sqlite', 'mags')
-	plt.scatter(old, new)
-	plt.savefig('mags_comparison')
+	#fig = plt.figure()
+	#old = db.dbUtils.getFromDB("r_mag", dbDir+'CALIFA.sqlite', 'gc', ' where califa_id < 100')
+	#new = db.dbUtils.getFromDB("mag", dbDir+'CALIFA.sqlite', 'mags')
+	#plt.scatter(old, new)
+	#plt.savefig('mags_comparison')
 	#sc_index = np.where(np.round(flux, 0) == round(flux[0]/math.e, 0))
         #sc_index = np.where([(np.divide(centralFlux, flux) > math.e) & (isoA > maxFluxIsoA)])[1][0] #the first index after the maximum	   
  	#lsc = isoA[sc_index]*0.396 #in arcseconds
 
 	#phys_lsc = cosm.angular2physical(lsc, z)
 	#print str(i)+","+ str(lsc)+","+ str(phys_lsc[0])+","+ str(z[0])#+","+str(maxFluxIsoA)+","+ str(lsc)# -- testing
-#np.savetxt("mags.csv", mags, fmt="%i,%f")
+np.savetxt("mags.csv", mags, fmt="%i,%f")
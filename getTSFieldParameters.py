7302c1a1fbd832b30d96c84629957948214d53ce
Astrolitterbox <opit@hardcore.lt>
2013-08-23 13:45:25 +0200

    drField files

--- getTSFieldParameters.py
+++ getTSFieldParameters.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-import pyfits
+#import pyfits
 import numpy as np
 import utils
-import inspect
+#import inspect
 import csv
 import string
 
@@ -59,10 +59,12 @@ def getParams(ID, band):
       field = GalaxyParameters.SDSS(ID).field
       runstr = GalaxyParameters.SDSS(ID).runstr
       field_str = GalaxyParameters.SDSS(ID).field_str
-	#http://das.sdss.org/imaging/5115/40/calibChunks/2/tsField-005115-2-40-0023.fit
+	
       #print 'STR -- http://das.sdss.org/imaging/'+run+'/'+rerun+'/calibChunks/'+camcol+'/tsField-'+runstr+'-'+camcol+'-'+rerun+'-'+field_str+'.fit'
       
       try:
+	print 'http://das.sdss.org/imaging/'+run+'/'+rerun+'/dr/'+camcol+'/drField-'+runstr+'-'+camcol+'-'+rerun+'-'+field_str+'.fit'
+	
 	tsFile = pyfits.open('http://das.sdss.org/imaging/'+run+'/'+rerun+'/calibChunks/'+camcol+'/tsField-'+runstr+'-'+camcol+'-'+rerun+'-'+field_str+'.fit', mode='readonly')
 
 	img = tsFile[1].data
@@ -70,23 +72,18 @@ def getParams(ID, band):
 	zpt_r = list(img.field(27))[0][filterNumber]
 	ext_coeff = list(img.field(33))[0][filterNumber]
 	airmass = list(img.field(22))[0][filterNumber]
-	
-	#print zpt_r, ext_coeff, airmass, '-------------------'
 	params = [zpt_r, ext_coeff, airmass]
       except IOError as err:
-	if filterNumber == 0:
-	  params = [-23.81, 0.50, 1.15]
-	elif filterNumber == 1:
-	  params = [-24.43, 0.18, 1.15]
-	elif filterNumber == 2:
-	  params = [-24.02, 0.10, 1.15]
-	elif filterNumber == 3:
-	  params = [-23.67, 0.06, 1.15]
-	elif filterNumber == 4:
-	  params = [-21.98, 0.06, 1.15]
-	print 'default params', err
+	tsFile = pyfits.open('http://das.sdss.org/imaging/'+run+'/'+rerun+'/dr/'+camcol+'/drField-'+runstr+'-'+camcol+'-'+rerun+'-'+field_str+'.fit', mode='readonly')	
+	img = tsFile[1].data
+	#indexing -- field numbering in documentation starts with 0, hence #27 instead of #28 field, etc
+	zpt_r = list(img.field(27))[0][filterNumber]
+	ext_coeff = list(img.field(33))[0][filterNumber]
+	airmass = list(img.field(22))[0][filterNumber]
+	params = [zpt_r, ext_coeff, airmass]
       return params
 
 
-
+for i in range(0, 940):
+  print getParams(i, 'r')
 
